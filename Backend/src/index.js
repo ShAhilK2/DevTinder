c1fdbee7d48de1831770133486973767fb650fb2
ShAhilK2 <shahil.kataria@invennico.com>
2025-03-26 10:34:45 +0530

    feat: Episode-07 | Diving into the APIs

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -8,19 +8,74 @@ const User = require("./models/user");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Mahi",
-    lastName: "Sharma",
-    age: 23,
-    email: "mahisharma6@gmail.com",
-    password: "mahisharma@88",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.status(201).send("User Created Successfully");
   } catch (err) {
     res.status(400).send("Error Saving the User", err.message);
+  }
+});
+
+// get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const foundUser = await User.find({ email: userEmail });
+
+    if (foundUser.length === 0) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.status(200).send({ "Found User": foundUser });
+    }
+  } catch (err) {
+    res.status(404).send("Something Went Wrong");
+  }
+});
+
+// Feed API - GET /feed => get all the users  from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+// Delete the User
+app.delete("/user", async (req, res) => {
+  try {
+    const { userId } = req.body;
+    await User.findByIdAndDelete({ _id: userId });
+
+    res.status(400).send("User Deleted Succesfully");
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
+  }
+});
+
+// Update the User
+app.patch("/user", async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const data = req.body;
+    console.log(data);
+    // await User.findByIdAndUpdate({ _id: userId }, data);
+
+    const userbefore = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "before",
+    });
+
+    console.log(userbefore);
+
+    res.status(400).send("User Updated Succesfully");
+  } catch (error) {
+    res.status(400).send("Something Went Wrong");
   }
 });
 
